1da1490d09feeb30226f06cbfc31f85ebdf9e49c
EssienAbasiama <essienabasiama11@gmail.com>
2023-09-07 00:12:48 -0800

    phase1 responsiveness update

--- src/Components/Template/LandPage/Phase1/Phase1.jsx
+++ src/Components/Template/LandPage/Phase1/Phase1.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "../../../UI/Button/Button";
 import { Microphone } from "../../../UI/constants";
 
 import styles from "./Phase1.module.css";
-import "./Phase1.module.css"
+import "./Phase1.module.css";
 
 const Phase1 = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    // Add a resize event listener to update the window width when the screen size changes
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Define your breakpoint (screen width) where the div should disappear
+  const breakpoint700 = 700; // Adjust this value to your desired screen width
+  const breakpoint641 = 641; // Adjust this value to your desired screen width
+
   return (
     <>
       <div className={styles.clinic}>
@@ -13,16 +32,54 @@ const Phase1 = () => {
           <div className={styles.grammercetamol_clinic}>
             <h2>THE GRAMMCETAMOL CLINIC</h2>
           </div>
-          <div className={styles.explain}>
+          {/* <div className={styles.explain}>
             <p>
               SPEAK <span>ENGLISH</span> MORE NATURALLY WITH <br /> CONFIDENCE
               AND STUNNING <span>DICTION</span>
             </p>
-          </div>
+          </div> */}
+          {windowWidth <= breakpoint700 && windowWidth > breakpoint641 ? (
+            <div className={styles.explain}>
+              <p>
+                SPEAK <span>ENGLISH</span> MORE NATURALLY WITH CONFIDENCE AND
+                STUNNING <span>DICTION</span>
+              </p>
+            </div>
+          ) : (
+            <div className={styles.explain}>
+              {windowWidth <= breakpoint641 ? (
+                <p>
+                  SPEAK <span>ENGLISH</span> MORE<br/> NATURALLY WITH
+                  <br/> CONFIDENCE AND<br/>STUNNING <span>DICTION</span>
+                </p>
+              ) : (
+                <p>
+                  SPEAK <span>ENGLISH</span> MORE NATURALLY WITH <br />
+                  CONFIDENCE AND STUNNING <span>DICTION</span>
+                </p>
+              )}
+            </div>
+          )}
           <div className={styles.title_text_container}>
-            <p className={styles.title_text_container_p}>Discover the Secrets to fat fluency,boost your word power<br></br>
+            {windowWidth <= breakpoint700 && windowWidth > breakpoint641 ? (
+              <p className={styles.title_text_container_p}>
+                Discover the Secrets to fat fluency,boost your word power and
+                improve your pronunciation
+              </p>
+              ) : (
+
+              
+              <>
+              {windowWidth <= breakpoint641 ? <p className={styles.title_text_container_p}>
+                Discover the Secrets to fat fluency,boost your word power
+                <br></br>
+                and improve your pronunciation
+              </p> : <p className={styles.title_text_container_p}>Discover the Secrets to fat fluency,boost your word power<br></br>
               and improve your pronunciation
-            </p>
+            </p>}
+              </>
+
+            )}
           </div>
           <Button className={styles.button}>Get Started</Button>
         </div>
